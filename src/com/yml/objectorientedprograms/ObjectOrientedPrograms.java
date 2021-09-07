91f33bcd7e9fafa2e11a31d5894be8f42af2cbe6
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-08 00:40:55 +0530

    Addedprogram for inventoryManagementSystem

--- src/com/yml/objectorientedprograms/ObjectOrientedPrograms.java
+++ src/com/yml/objectorientedprograms/ObjectOrientedPrograms.java
@@ -1,11 +1,14 @@
 package com.yml.objectorientedprograms;
 
 import com.yml.inventory.JsonInventory;
+import com.yml.inventorymanagementsystem.InventoryManager;
 
 public class ObjectOrientedPrograms {
 
 	public static void main(String[] args) {
-		JsonInventory.jsonInventory() ;
+//		JsonInventory.jsonInventory() ;
+		
+		InventoryManager.inventoryManager();
 
 	}
 
