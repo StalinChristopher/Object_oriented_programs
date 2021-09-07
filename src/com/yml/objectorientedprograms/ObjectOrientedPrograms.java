0ed6f28142dd3ec424023276bbd118eaa74f3398
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-08 00:00:15 +0530

    Created seperate package for main program

---
+++ src/com/yml/objectorientedprograms/ObjectOrientedPrograms.java
@@ -0,0 +1,12 @@
+package com.yml.objectorientedprograms;
+
+import com.yml.inventory.JsonInventory;
+
+public class ObjectOrientedPrograms {
+
+	public static void main(String[] args) {
+		JsonInventory.jsonInventory() ;
+
+	}
+
+}
